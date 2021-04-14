9d4d636bbd355e4b713db1936502727e4053cc48
Piotr Kosko/Tizen API (PLT) /SRPOL/Engineer/Samsung Electronics <p.kosko@samsung.com>
2021-04-14 10:18:21 +0200

    [Clipboard] setData and getData implementations added
    and also removed code for hasStrings, as this API is not present in documentation.
    
    [Before] Clipboard.setData and Clipboard.getData feature was not implemented
    
    [After] Naive implementation of clipboard shared only inside Flutter application
    and only via Clipboard API. Such decision was done because of supporting partial
    functionality on all profiles. CBHM API is supported only on mobile and currently
    it is not possible to support it in other way.
    
    Dart code below is able to set/get internal clipboard data:
          ClipboardData cd = ClipboardData(text: 'some text');
          Clipboard.setData(cd);
    
          Future<ClipboardData?> d = Clipboard.getData(Clipboard.kTextPlain);
          void dataCallback(ClipboardData? d) {
            if (d != null) {
              String? text = d.text;
              if(text != null) {
                print("Clipboard data $text");
              }
            }
          }
          d.then(dataCallback);

--- shell/platform/tizen/channels/platform_channel.cc
+++ shell/platform/tizen/channels/platform_channel.cc
@@ -5,6 +5,7 @@
 #include "platform_channel.h"
 
 #include <app.h>
+#include <mutex>
 
 #include "flutter/shell/platform/common/cpp/json_method_codec.h"
 #include "flutter/shell/platform/tizen/tizen_log.h"
@@ -13,7 +14,9 @@ static constexpr char kChannelName[] = "flutter/platform";
 
 PlatformChannel::PlatformChannel(flutter::BinaryMessenger* messenger)
     : channel_(std::make_unique<flutter::MethodChannel<rapidjson::Document>>(
-          messenger, kChannelName, &flutter::JsonMethodCodec::GetInstance())) {
+          messenger,
+          kChannelName,
+          &flutter::JsonMethodCodec::GetInstance())) {
   channel_->SetMethodCallHandler(
       [this](
           const flutter::MethodCall<rapidjson::Document>& call,
@@ -29,6 +32,7 @@ void PlatformChannel::HandleMethodCall(
     std::unique_ptr<flutter::MethodResult<rapidjson::Document>> result) {
   const auto method = call.method_name();
 
+  FT_LOGI("pkosko Called method : %s", method.c_str());
   if (method == "SystemNavigator.pop") {
     ui_app_exit();
     result->Success();
@@ -37,11 +41,9 @@ void PlatformChannel::HandleMethodCall(
   } else if (method == "HapticFeedback.vibrate") {
     result->NotImplemented();
   } else if (method == "Clipboard.getData") {
-    result->NotImplemented();
+    Clipboard::GetData(call, std::move(result));
   } else if (method == "Clipboard.setData") {
-    result->NotImplemented();
-  } else if (method == "Clipboard.hasStrings") {
-    result->NotImplemented();
+    Clipboard::SetData(call, std::move(result));
   } else if (method == "SystemChrome.setPreferredOrientations") {
     result->NotImplemented();
   } else if (method == "SystemChrome.setApplicationSwitcherDescription") {
@@ -57,3 +59,65 @@ void PlatformChannel::HandleMethodCall(
     result->NotImplemented();
   }
 }
+
+// Clipboard constants and variables
+namespace Clipboard {
+
+// naive implementation using std::string as a container of internal clipboard
+// data
+std::mutex is_processing_mutex;
+std::string string_clipboard = "";
+
+static constexpr char kTextKey[] = "text";
+static constexpr char kTextPlainFormat[] = "text/plain";
+static constexpr char kUnknownClipboardFormatError[] =
+    "Unknown clipboard format error";
+static constexpr char kUnknownClipboardError[] =
+    "Unknown error during clipboard data retrieval";
+
+void GetData(
+    const flutter::MethodCall<rapidjson::Document>& call,
+    std::unique_ptr<flutter::MethodResult<rapidjson::Document>> result) {
+  const rapidjson::Value& format = call.arguments()[0];
+
+  // https://api.flutter.dev/flutter/services/Clipboard/kTextPlain-constant.html
+  // API supports only kTextPlain format
+  if (strcmp(format.GetString(), kTextPlainFormat) != 0) {
+    result->Error(kUnknownClipboardFormatError,
+                  "Clipboard API only supports text.");
+    return;
+  }
+
+  {
+    std::unique_lock<std::mutex> lock(is_processing_mutex);
+    if (string_clipboard.empty()) {
+      result->Error(kUnknownClipboardError, "No clipboard data available.");
+    } else {
+      rapidjson::Document document;
+      document.SetObject();
+      rapidjson::Document::AllocatorType& allocator = document.GetAllocator();
+      document.AddMember(rapidjson::Value(kTextKey, allocator),
+                         rapidjson::Value(string_clipboard, allocator),
+                         allocator);
+      result->Success(document);
+    }
+  }
+}
+
+void SetData(
+    const flutter::MethodCall<rapidjson::Document>& call,
+    std::unique_ptr<flutter::MethodResult<rapidjson::Document>> result) {
+  const rapidjson::Value& document = *call.arguments();
+  rapidjson::Value::ConstMemberIterator itr = document.FindMember(kTextKey);
+  if (itr == document.MemberEnd()) {
+    result->Error(kUnknownClipboardError, "Invalid message format");
+    return;
+  }
+  {
+    std::unique_lock<std::mutex> lock(is_processing_mutex);
+    string_clipboard = itr->value.GetString();
+    FT_LOGI("pkosko Set value of clipboard : %s", string_clipboard.c_str());
+    result->Success();
+  }
+}
+}  // namespace Clipboard
